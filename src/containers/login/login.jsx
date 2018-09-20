cf5b1c3235b761a22b95b443c401f895df7d9dac
houxiaoyu <1531604938@qq.com>
2018-09-20 08:49:12 +0800

    updata

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -1,9 +1,82 @@
-//登录路由界面
 import React, {Component} from 'react'
-export default class Login extends Component {
-  render () {
+import {NavBar, WingBlank, List, WhiteSpace, InputItem, Button} from 'antd-mobile'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+
+import {login} from '../../redux/actions'
+import Logo from '../../componnets/logo/logo'
+
+/*
+登陆路由组件
+ */
+class Login extends Component {
+
+  state = {
+    username: '',
+    password: '',
+  }
+
+  // 处理输入发生改变的监听回调
+  handleChange = (name, value) => {
+    this.setState({
+      [name]: value
+    })
+  }
+
+  // 登陆的回调
+  login = () => {
+    // console.log(this.state)
+    const {username, password} = this.state
+    this.props.login(username, password)
+  }
+
+  goRegister = () => {
+    // 编程式路由导航(跳转)
+    this.props.history.replace('/register')
+  }
+
+  render() {
+
+    const {redirectTo, msg} = this.props.user
+    if(redirectTo) {
+      return <Redirect to={redirectTo}/>
+    }
+
     return (
-      <div>login</div>
+      <div>
+        <NavBar>硅谷直聘</NavBar>
+        <Logo/>
+        <WingBlank>
+          <List>
+            {msg ? <p className='error-msg'>{msg}</p> : null}
+            <InputItem
+              placeholder='请输入用户名'
+              onChange={val => this.handleChange('username', val)}
+            >
+              用户名:
+            </InputItem>
+            <WhiteSpace/>
+
+            <InputItem
+              type='password'
+              placeholder='请输入密码'
+              onChange={val => this.handleChange('password', val)}
+            >
+              密码:
+            </InputItem>
+            <WhiteSpace/>
+
+            <Button type='primary' onClick={this.login}>登&nbsp;&nbsp;陆</Button>
+            <WhiteSpace/>
+            <Button onClick={this.goRegister}>还没有账户</Button>
+          </List>
+        </WingBlank>
+      </div>
     )
   }
 }
+
+export default connect(
+  state => ({user: state.user}),
+  {login}
+)(Login)
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,9 +4,13 @@ reducer函数: 根据老的state和指定的action 生产新的state
  */
 import {combineReducers} from 'redux'
 
+import {getRedirectPath} from '../utils'
+
 import {
   AUTH_SUCCESS,
-  ERROR_MSG
+  ERROR_MSG,
+  RECEIVE_USER,
+  RESET_USER
 } from './action-types'
 
 // 管理user数据
@@ -20,14 +24,20 @@ function user (state=initUser, action) {
   switch (action.type) {
     case AUTH_SUCCESS:
       const user = action.data
-      return {...user, redirectTo: '/'}
+      return {...user, redirectTo: getRedirectPath(user.type, user.header)}
     case ERROR_MSG:
       const msg = action.data
       return {...state, msg}
+    case RECEIVE_USER:
+      return action.data
+    case RESET_USER:
+      return {...initUser, msg: action.data}
     default:
       return state
   }
 }
+
+
 
 
 export default combineReducers({
